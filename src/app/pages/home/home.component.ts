6356adcddbc9bcb06fff668f7bd8d802c2f697cf
amani1505 <amanideveloper1505@gmail.com>
2024-07-27 09:00:59 +0300

    Adding mission component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { ObjectivesComponent } from '../../components/objectives/objectives.component';
 import { LazyloadDirective } from '../../directives/lazyload.directive';
+import { MissionComponent } from '../../components/mission/mission.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent,ObjectivesComponent,LazyloadDirective],
+  imports: [
+    HeroComponent,
+    ObjectivesComponent,
+    LazyloadDirective,
+    MissionComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
