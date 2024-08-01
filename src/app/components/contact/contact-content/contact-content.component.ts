b3017561536692faafd0f090b64ea18be5ec51b7
amani1505 <amanideveloper1505@gmail.com>
2024-08-01 14:18:41 +0300

    adding Contact Page

---
+++ src/app/components/contact/contact-content/contact-content.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
+@Component({
+  selector: 'app-contact-content',
+  standalone: true,
+  imports: [AngularSvgIconModule],
+  templateUrl: './contact-content.component.html',
+  styleUrl: './contact-content.component.scss'
+})
+export class ContactContentComponent {
+
+}
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/contact/header/header.component';
+import { ContactContentComponent } from '../../components/contact/contact-content/contact-content.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,ContactContentComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
